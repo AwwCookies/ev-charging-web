18b6833dcd2d7c8a9bec6aae7da1562f5aba431f
AwwCookies <emma@emaw.dev>
2023-04-19 14:47:03 -0400

    test cloudflare functions part 2

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,8 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest(context) {
+    const { results } = await context.env.text.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?"
+      )
+        .bind("Bs Beverages")
+        .all();
+    return new Response(results)
 }
