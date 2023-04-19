393ec0cedb50eab27d2b8f7c11cf4a8090727053
AwwCookies <emma@emaw.dev>
2023-04-19 15:07:42 -0400

    ??

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     // Create a prepared statement with our query
-    const ps = context.env
+    const ps = context.env.TEST.prepare('SELECT * FROM Customers');
   
     return Response.json(ps);
   }
